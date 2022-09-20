7dbff6275e8910679ba45667386f7ca1793315b3
Ian Alexander Wood <ianalexanderwood@gmail.com>
2022-09-20 03:14:47 +0100

    more tests

--- test/integration/k6.js
+++ test/integration/k6.js
@@ -12,6 +12,7 @@ export default function () {
       "status code should be 200": res => res.status === 200,
     });
   });
+
   group('GET /users', () => {
     const response = http.get('http://0.0.0.0:8080/users');
     check(response, {
@@ -46,6 +47,67 @@ export default function () {
     });
     check(response, {
       "response should have 0 users": res => res.json().length === 0,
+    });
+  });
+
+  group('POST /user', () => {
+    const response = http.post('http://0.0.0.0:8080/user');
+    check(response, {
+      "status code should be 400": res => res.status === 400,
+    });
+  });
+
+  group('POST /user with email', () => {
+    const payload = {
+      email: 'test@testing.com'
+    };
+    const response = http.post('http://0.0.0.0:8080/user', payload);
+    check(response, {
+      "status code should be 201": res => res.status === 201,
+    });
+  });
+
+  group('POST /user with duplicate email', () => {
+    const payload = {
+      email: 'test@testing.com'
+    };
+    const response = http.post('http://0.0.0.0:8080/user', payload);
+    check(response, {
+      "status code should be 500": res => res.status === 500,
+    });
+  });
+
+  group('PATCH /user/:id', () => {
+    const response = http.patch('http://0.0.0.0:8080/user/5');
+    check(response, {
+      "status code should be 400": res => res.status === 400,
+    });
+  });
+
+  group('PATCH /user/:id with valid kv pairs', () => {
+    const payload = {
+      first_name: 'Tom',
+      last_name: 'Hanks',
+      email: 'tom@hanks.com'
+    };
+    const response = http.patch('http://0.0.0.0:8080/user/5', payload);
+    check(response, {
+      "status code should be 200": res => res.status === 200,
+    });
+  });
+
+  group('DELETE /user/:id', () => {
+    const response = http.del('http://0.0.0.0:8080/user/5');
+    check(response, {
+      "status code should be 200": res => res.status === 200,
+    });
+  });
+
+  group('DELETE /user/:id with an invalid id', () => {
+    const response = http.del('http://0.0.0.0:8080/user/notanid');
+    console.log(response.body);
+    check(response, {
+      "status code should be 500": res => res.status === 500,
     });
   });
 }
